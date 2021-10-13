7091d0db213db0cdd985a2ec5c0bf7e51b96cda8
unknown <jharinigowda@gmail.com>
2021-10-13 21:15:39 +0530

    first commit

--- register.php
+++ register.php
@@ -1,85 +1,63 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head> 
-        <title> LOGIN PAGE </title>
-        <?php include "includes\header.php"; ?>
-        
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+        <title>Register Page</title>
+        <?php include "includes\header.php"; ?> 
         <style>
-             .navbar-nav{
+            .login-section-logo{
+                padding-top: 20px !important;
+                padding-bottom: 20px !important;
+            }
+
+            .top-spacing{
+                padding-top: 10px !important;
+            }
+
+            .navbar-nav {
                 flex-direction: row;
             }
-            .nav-item{
-                padding-left: 10px !important
-            }
-         </style>
-
+        </style>
     </head>
-
     <body>
-        <header> 
-        <?php include "includes\landing_nav.php"; ?>
-        </header> 
-
-
-            <center>
-           
-                <div class="col-sm-12 col-sm-12 col-lg-4">
-                    <div class="card" style="width: 25rem;">
-                        <div class="card-body">
-                            <div class="text-center">
-                                <form>
-                                    <a class="navbar-brand" href="#">   
-                                        <img src="assets4/images4/cbitlogo.jfif" width="50" height="50" alt="LOGO">
-                                     </a> <br>
-                                     <p><b>USER LOGIN FORM </b></p>
-                                     <p>Please fill in your credentials to continue </p>
-                                     <div class="form-group">
-                                    <input class="form-control" type="text" name="Enter Email id" id="user_email" placeholder="Enter Your name">
-                                    <span id="name_error" class="text-danger"></span>
-                                    <br> 
-                                    <input class="form-control" type="phone number name="phone number"  id="user_email" placeholder="Enter Your phone number">
-                                    <span id="phone number-error" class="text-danger"></span>
-                                    <br> 
-                                    <input class="form-control" type="email" name="email" placeholder="Enter Your email">
-                                    <span id="email-error" class="text-danger"></span>
-                                    <br>
-                                    <input class="form-control" type="password" name="password" placeholder="Enter Your password">
-                                    <span id="password_error" class="text-danger"></span>
-                                    <br>
-                                    <input class="form-control" type="confirm password" name=" confirm password" placeholder="confirm password">
-                                    <span id="confirm password-error" class="text-danger" ></span>
-                                    <br>
-                                    
-                                    <input type="submit" class="btn btn-block btn-success" value="SIGN UP">
-                                    <br> 
-                                    <label> Already have an account? 
-                                        <a href="login.php"> login instead </a> 
-                                     </label>
-                                     
-                                </form>
-                            </div>
-                        </div>
+        <header>
+            <?php include "includes\landing_nav.php"; ?>
+        </header>
+        <center>
+        <div class="card" style="width: 450px;top:50px">
+            <div class="container">
+                <img src="assets4\images4\cbitlogo.jfif" width="75px" class="login-section-logo">
+                <h5>USER REGISTRATION FORM</h5>
+                <P>Please fill in your credentials to continue</P>
+                <form method="post" action="server.php">
+                    <input type="hidden" name="operation_type" value="register">
+                    <div class="form-group top-spacing">
+                        <input class="form-control" type="text" name="name" id="user_name" placeholder="Enter Your Name">
                     </div>
-                </div>
-            </center>
-            <script>
-         $("#user_email").on('keyup',function(){
-             if($("#user_email").val()==''){
-                 $("#email_error").text('email is required');
-             }else{
-                 $("#email_error").text('');
-             }
-         });
-
-
-         $("#user_password").on('keyup',function(){
-             if($("#user_password").val()==''){
-                 $("#password_error").text('password is required');
-             }else{
-                 $("#password_error").text('');
-             }
-         });
-</script>
-
+                    <div class="form-group top-spacing">
+                        <input class="form-control" type="text" name="phone_no" id="user_phone_no" placeholder="Enter Your Phone No.">
+                    </div>
+                    <div class="form-group top-spacing">
+                        <input class="form-control" type="text" name="email" id="user_email" placeholder="Enter Your Email">
+                    </div>
+                    <div class="form-group top-spacing">
+                        <input  class="form-control" type="password" name="password" id="user_password" placeholder="Enter Your Password">
+                    </div>
+                    <div class="form-group top-spacing">
+                        <input  class="form-control" type="password" name="password_confirm" id="user_password_confirmation" placeholder="Confirm Your Password">
+                    </div>
+                    
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-info btn-block" style="background-color: #2221F7 !important;" value="Signup">
+                    </div>
+                    <br>
+                    <p>Already have an account? <a href="index.php">Login instead</a></p>
+                    
+                </form>
+            </div>
+        </div>
+        <center>
+       <?php include "includes\\footer.php"; ?>
     </body>
 </html>
